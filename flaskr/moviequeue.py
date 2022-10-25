b1bd85bf910f8fb9c23bcd5dcebf52a7a8522d84
CharlesPizza <85193781+CharlesPizza@users.noreply.github.com>
2022-10-25 06:31:06 -0400

    Added upvote function

--- flaskr/moviequeue.py
+++ flaskr/moviequeue.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, session
 )
 from werkzeug.exceptions import abort
 
@@ -20,14 +20,39 @@ def index():
         db.execute('INSERT OR IGNORE INTO queue (movie_id, title)'
             ' VALUES (?, ?)', (int(add_movies[0]), add_movies[1]),
             )
-        db.commit()
+        db.execute('INSERT OR IGNORE INTO votes (movie_id)'
+            ' VALUES ((SELECT movie_id FROM queue WHERE movie_id = ?))', (int(add_movies[0]),),
+            )
+        print(int(add_movies[0]))
 
+        db.commit()
     movies = db.execute('SELECT queue.movie_id, queue.title, COUNT(votes.user_id) as count'
         ' FROM votes CROSS JOIN queue ON votes.movie_id = queue.movie_id'
-        ' GROUP BY votes.movie_id;'
+        ' GROUP BY votes.movie_id ORDER BY count DESC;'
         ).fetchall()
-
+    print(type(movies))
     return render_template('index.html', movies=movies)
+
+@bp.route('/vote', methods=('GET','POST'))
+@login_required
+def vote():
+    db = get_db()
+    if request.method == 'GET':
+        print('OOPS')
+    if request.method == 'POST':
+        user_id = session['user_id']
+        r = db.execute('SELECT * FROM votes WHERE (movie_id, user_id) = (?, ?)', (int(request.form['vote']), int(user_id))).fetchone()
+        print(r)
+        if r is None:
+            db.execute('INSERT INTO votes (movie_id, user_id)'
+                ' VALUES (?, ?)', (int(request.form['vote']), int(user_id)))
+        else:
+            db.execute('DELETE FROM votes WHERE movie_id = ? AND user_id = ?', (int(request.form['vote']), int(user_id)))
+        db.commit()
+
+        print(session['user_id'])
+        print(request.form['vote'])
+    return redirect('/')
 
 @bp.route('/search', methods=('GET', 'POST'))
 @login_required
